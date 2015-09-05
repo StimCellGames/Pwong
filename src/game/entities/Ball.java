e9cbcdd46e8cadf5d855efad57568140a3c00eb2
Zormion <Zormion@gmail.com>
2015-09-04 22:51:23 -0400

    Entity Stuff Done

--- src/game/entities/Ball.java
+++ src/game/entities/Ball.java
@@ -1,5 +1,23 @@
 package game.entities;
 
-public class Ball {
+import java.awt.Graphics;
+
+public class Ball extends Entity {
+
+	@Override
+	public void init() {
+		width = 15;
+		height = 15;
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawOval(x, y, width, height);
+	}
 
 }
--- src/game/entities/Entity.java
+++ src/game/entities/Entity.java
@@ -1,5 +1,18 @@
 package game.entities;
 
-public class Entity {
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+public abstract class Entity {
+	
+	protected int x, y;
+	protected int width, height;
+	
+	public abstract void init();
+	public abstract void update();
+	public abstract void render(Graphics g);
+	
+	public Rectangle bounds() {
+		return new Rectangle(x, y, width, height);
+	}
 }
