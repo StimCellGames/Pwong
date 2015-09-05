d9766c99e74aaeb8be982a0ace88522b97f5e4a7
Zormion <Zormion@gmail.com>
2015-09-04 23:06:51 -0400

    entity stuff

--- src/game/entities/Entity.java
+++ src/game/entities/Entity.java
@@ -1,5 +1,17 @@
 package game.entities;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
 public abstract class Entity {
+	protected int x, y;
+	protected int width, height;
 	
+	public abstract void init();
+	public abstract void update();
+	public abstract void render(Graphics g);
+	
+	public Rectangle bounds() {
+		return new Rectangle(x, y, width, height);
+	}
 }
--- src/game/entities/Paddle.java
+++ src/game/entities/Paddle.java
@@ -1,5 +1,22 @@
 package game.entities;
 
-public class Paddle {
+import java.awt.Graphics;
 
+public class Paddle extends Entity {
+	
+	@Override
+	public void init() {
+		width = 20;
+		height = 30;
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawRect(x, y, width, height);
+	}
 }
